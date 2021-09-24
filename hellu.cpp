5651845bc3ac7aa4d0a905c4fa8d016471385f49
luongviethoang <91303390+luongviethoang@users.noreply.github.com>
2021-09-24 12:39:30 +0700

    Create hellu.cpp

---
+++ hellu.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char** argv) {
+	printf("hoang oi\n");
+	printf("hoang oi lam sao h\n");
+	printf("da 1 nam troi qua\n");
+	printf("nguoi yeu van chua co\n");
+	printf("cho thi van chua nuoi\n");
+	printf("dem den lai buon thui\n");
+	printf("chui duc trong phong hoc");
+	return 0;
+}
