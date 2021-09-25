bd20bd0843682e4fd7690c690de04224e8b3baee
your name <hoangckhg170@gmail.com>
2021-09-25 21:59:28 +0700

    Create ss2.3.c

---
+++ ss2.3.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char *argv[]) {
+	int b = 9 ;
+	int c = 10 ;
+	int d;
+	d = b + c;
+	printf("chao hoang\n");
+	printf("d = %e\n", d);
+	printf("%e + %e = %e",b,c,d);
+	return 0;
+}
