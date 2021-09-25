81466eccf7ac2880b861b8f175af62f9ea4e6176
your name <hoangckhg170@gmail.com>
2021-09-25 22:00:05 +0700

    Create ss3.3.c

---
+++ ss3.3.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char *argv[]) {
+	int a, b, c, sum;
+	printf("hihi xin chao\n");
+	printf("\n nhap so bat ki:");
+	scanf("%d %d %d", &a, &b, &c);
+	sum = a + b + c;
+	printf("\n Sum = %d", sum);
+	return 0;
+}
