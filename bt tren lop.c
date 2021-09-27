25a5be51f72b231ac22ed746833bb9ce1fca5c76
your name <hoangckhg170@gmail.com>
2021-09-27 09:44:57 +0700

    Create bt tren lop.c

---
+++ bt tren lop.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(void) {
+	float a;
+	float b;
+    float c;
+	printf("nhap so dau tien\n");
+	scanf("%f", &a);
+	printf("nhap so thu 2\n");
+	scanf("%f", &b);
+	printf("nhap so thu 3\n");
+	scanf("%f", &c);
+	printf("Sum is %f\n", a + b + c);
+	printf("avg is %f", (a + b + c)/3);
+	
+	
+	
+	
+ 
+}
